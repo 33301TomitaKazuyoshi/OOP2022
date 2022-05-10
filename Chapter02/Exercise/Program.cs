899d0a86105472b142d9b47f595c43005af8cf3e
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-05-10 14:18:31 +0900

    問題2.1.3完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+            //Songクラスのインスタンスを複数作成し、配列songsに格納
+            List<Song> songs = new List<Song>();
+
         }
     }
 }
