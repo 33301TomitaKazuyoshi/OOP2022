252c1170b043d0a6d31a91efe5bb8fdd1d66f56a
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-05-12 13:38:47 +0900

    クラスInchConverterの作成

---
+++ Chapter02/Exercise/InchConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise {
+    public static class InchConverter {
+
+        //定数をratioで定義
+        private const double ratio = 0.0254;  //マジックナンバー
+        //public static readonly double ratio = 0.0254 ⇐外部からもアクセスさせたい場合
+        //メートルからフィートを求める(静的メソッド)
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+        //フィートからメートルを求める(静的メソッド)
+        public static double ToMeter(int inch) {
+            return inch * ratio;
+        }
+    }
+}
+}
