5cb55d4fe2b740b21c3a12b2631cd69518c1dbb5
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-05-20 13:16:30 +0900

    Section01完成

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            var removeCount = list.RemoveAll(s => s.Contains("on"));
+            Console.WriteLine(removeCount);
+            list.ForEach(s => Console.WriteLine(s));
+
+
+            //foreach (var s in names) {
+            //    Console.WriteLine(s);
+            //} 
+
+        }
+    }
+}
