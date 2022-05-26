aa13484ab1a9a53f17b81cc85f1094c4f730b22b
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-05-26 15:23:20 +0900

    演習問題5.1完成

--- Chapter05/Exercise01/Program.cs
+++ Chapter05/Exercise01/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+
+            Console.Write("文字列１：");
+            var inputString1 = Console.ReadLine();
+
+            Console.Write("文字列２：");
+            var inputString2 = Console.ReadLine();
+
+            if (String.Compare(inputString1,inputString2,true) == 0) {
+                Console.WriteLine("文字列は等しい。");
+            }
         }
     }
 }
