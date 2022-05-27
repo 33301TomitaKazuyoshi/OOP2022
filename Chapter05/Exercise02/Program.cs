25c9769e1965fc3894d6d93ad7c2ca04fcf15f21
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-05-27 13:37:45 +0900

    演習問題5.2完成

--- Chapter05/Exercise02/Program.cs
+++ Chapter05/Exercise02/Program.cs
@@ -8,7 +8,15 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
+            var line = Console.ReadLine();
+            int num;  //整数値を入れる変数を用意
 
+            //P126
+            if (int.TryParse(line, out num)) {
+                Console.WriteLine("{0:#,#}", num);  //整数への変換成功
+            } else {
+                Console.WriteLine("数字文字列ではありません"); //変換失敗
+            }
         }
     }
 }
