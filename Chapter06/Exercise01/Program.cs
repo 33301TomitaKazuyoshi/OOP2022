c7cfdd8351563998a63326b0c5a9a4d2f524bb1a
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-06-02 14:01:59 +0900

    演習問題6.1完成

--- Chapter06/Exercise01/Program.cs
+++ Chapter06/Exercise01/Program.cs
@@ -23,10 +23,10 @@ namespace Exercise01 {
 
             Exercise1_5(numbers);
         }
+
         //最大値
         private static void Exercise1_1(int[] numbers) {
-            var max = numbers.Max();
-            Console.WriteLine(max);
+            Console.WriteLine(numbers.Max());
         }
         //最後から２つの要素取り出し
         private static void Exercise1_2(int[] numbers) {
@@ -35,15 +35,16 @@ namespace Exercise01 {
             }
         }
         //数値を文字列に変換
-        private static void Exercise1_3(int[] numbers) {
-            foreach (var items in numbers.Select(n => n)) {
-                Console.WriteLine(items);
+        private static void Exercise1_3(int[] numbers) { 
+            foreach (var strs in numbers.Select(n => n.ToString())) {
+                Console.WriteLine(strs);
             }
         }
         //小さい順に並べて先頭の３つを出力
         private static void Exercise1_4(int[] numbers) {
-            var sortedNumbers = numbers.OrderBy(n => n);
-            Console.WriteLine();
+            foreach (var n in numbers.OrderBy(n => n).Take(3)) {
+                Console.WriteLine(n);
+            }
         }
         //重複を排除し１０より大きい数がいくつあるか
         private static void Exercise1_5(int[] numbers) {
