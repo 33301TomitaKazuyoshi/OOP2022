44f69493238a53ee3213d14d53fadd2d4bd7f1cb
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-06-07 13:15:02 +0900

    計算のフォームアプリケーションの作成　6月3日分

---
+++ FormAppSample/Sample0603/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Sample0603 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btPush = new System.Windows.Forms.Button();
+            this.tbAns = new System.Windows.Forms.TextBox();
+            this.tbSuu1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudSuu1 = new System.Windows.Forms.NumericUpDown();
+            this.tbSuu2 = new System.Windows.Forms.TextBox();
+            this.nudSuu2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudAns = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSuu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSuu2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAns)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btPush
+            // 
+            this.btPush.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btPush.Font = new System.Drawing.Font("OCR A Extended", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPush.Location = new System.Drawing.Point(239, 115);
+            this.btPush.Name = "btPush";
+            this.btPush.Size = new System.Drawing.Size(198, 104);
+            this.btPush.TabIndex = 0;
+            this.btPush.Text = "計算";
+            this.btPush.UseVisualStyleBackColor = false;
+            this.btPush.Click += new System.EventHandler(this.btPush_Click);
+            // 
+            // tbAns
+            // 
+            this.tbAns.Location = new System.Drawing.Point(496, 51);
+            this.tbAns.Name = "tbAns";
+            this.tbAns.Size = new System.Drawing.Size(107, 19);
+            this.tbAns.TabIndex = 1;
+            this.tbAns.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbSuu1
+            // 
+            this.tbSuu1.Location = new System.Drawing.Point(97, 51);
+            this.tbSuu1.Name = "tbSuu1";
+            this.tbSuu1.Size = new System.Drawing.Size(96, 19);
+            this.tbSuu1.TabIndex = 1;
+            this.tbSuu1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(221, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(443, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "=";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nudSuu1
+            // 
+            this.nudSuu1.Location = new System.Drawing.Point(25, 276);
+            this.nudSuu1.Name = "nudSuu1";
+            this.nudSuu1.Size = new System.Drawing.Size(194, 19);
+            this.nudSuu1.TabIndex = 3;
+            this.nudSuu1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // tbSuu2
+            // 
+            this.tbSuu2.Location = new System.Drawing.Point(286, 58);
+            this.tbSuu2.Name = "tbSuu2";
+            this.tbSuu2.Size = new System.Drawing.Size(119, 19);
+            this.tbSuu2.TabIndex = 1;
+            this.tbSuu2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // nudSuu2
+            // 
+            this.nudSuu2.Location = new System.Drawing.Point(362, 276);
+            this.nudSuu2.Name = "nudSuu2";
+            this.nudSuu2.Size = new System.Drawing.Size(194, 19);
+            this.nudSuu2.TabIndex = 3;
+            this.nudSuu2.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(282, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "+";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(596, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "=";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nudAns
+            // 
+            this.nudAns.Location = new System.Drawing.Point(644, 276);
+            this.nudAns.Name = "nudAns";
+            this.nudAns.Size = new System.Drawing.Size(194, 19);
+            this.nudAns.TabIndex = 3;
+            this.nudAns.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 450);
+            this.Controls.Add(this.nudAns);
+            this.Controls.Add(this.nudSuu2);
+            this.Controls.Add(this.nudSuu1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSuu2);
+            this.Controls.Add(this.tbSuu1);
+            this.Controls.Add(this.tbAns);
+            this.Controls.Add(this.btPush);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSuu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSuu2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAns)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btPush;
+        private System.Windows.Forms.TextBox tbAns;
+        private System.Windows.Forms.TextBox tbSuu1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudSuu1;
+        private System.Windows.Forms.TextBox tbSuu2;
+        private System.Windows.Forms.NumericUpDown nudSuu2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudAns;
+    }
+}
+
---
+++ FormAppSample/Sample0603/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0603 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        //ボタンクリックイベントハンドラ
+        private void btPush_Click(object sender, EventArgs e) {
+            tbAns.Text = (int.Parse(tbSuu1.Text) + int.Parse(tbSuu2.Text)).ToString();
+            nudAns.Value = nudSuu1.Value + nudSuu2.Value;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e) 
+            {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) {
+            
+        }
+    }
+}
