db415e73b0d6ed31f8fa14696ff847927d96d9f5
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-06-14 14:29:39 +0900

    途中経過

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -3,37 +3,57 @@ using System.IO;
 
 namespace Test01 {
     class ScoreCounter {
-        private IEnumerable<Student> _score;
+        private IEnumerable<Student> _StudentScore;
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _StudentScore = ReadScore(filePath);
 
 
 
-            
         }
 
 
-        //メソッドの概要： 
+        //売り上げデータを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            List<Student> scores = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student sale = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(sale);
+            }
+            return scores;
         }
 
-        //メソッドの概要： 
-        public IDictionary<string, int> GetPerStudentScore() {
 
 
 
 
 
-            
-        }
+
     }
+
+            //店舗別売り上げを求める
+    public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (Student sale in _StudentScore) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    dict[sale.ShopName] = sale.Amount;
+
+            }
+            return dict;
+        }
+
+
+
+
+
 }
+ 
