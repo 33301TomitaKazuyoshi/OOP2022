d07c6f1b680eb6648e72654b82f26ade7bd2d4df
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-08-26 11:45:45 +0900

    Section01

--- Chapter17/Section01/Program.cs
+++ Chapter17/Section01/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+
+            var greetings = new List<IGreeting>() {
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+            };
+            foreach (var obj in greetings) {
+                string msg = obj.GetMessage();
+                Console.WriteLine(msg);
+            }
         }
     }
 }
