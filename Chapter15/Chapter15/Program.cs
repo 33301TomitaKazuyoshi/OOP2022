8464cf531c00fd0d911f41f4356ab27b8c65d7ef
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-09-29 15:53:54 +0900

    Chapter15途中経過①

--- Chapter15/Chapter15/Program.cs
+++ Chapter15/Chapter15/Program.cs
@@ -7,7 +7,17 @@ using System.Threading.Tasks;
 namespace Chapter15 {
     class Program {
         static void Main(string[] args) {
-
+            var price = Library.Books.Where(b=>b.CategoryId == 1).Max(b=>b.Price);
+            var query1 = Library.Books.Select(b => b.PublishedYear).Distinct().OrderBy(y => y);
+            var query2 = Library.Books.OrderBy(b => b.CategoryId).ThenByDescending(b => b.PublishedYear);
+            Console.WriteLine(price);
+            Console.WriteLine();
+            foreach (var n in query2) { 
+                Console.WriteLine(n); 
+            }
+            
+        
+        
         }
     }
 }
