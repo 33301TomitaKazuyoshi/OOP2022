7fe564f869f4b1c169728c2f1fdda5697092ed62
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-11-10 10:33:16 +0900

    DeleteButton追加

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -34,8 +34,7 @@ namespace ColorChecker {
                 .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
         }
         private void ColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            colorLabel.Background = new SolidColorBrush( Color.FromRgb((byte)RedSlider.Value, (byte)GreenSlider.Value, (byte)BlueSlider.Value));
-            
+            setColor();
         }
 
         private void Border_Loaded(object sender, RoutedEventArgs e) {
@@ -45,13 +44,6 @@ namespace ColorChecker {
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             var myColor = (MyColor)((ComboBox)sender).SelectedItem;
             var color = myColor.Color;
-            var name = myColor.Name;
-
-            stockMyColor.Add(new MyColor() {
-                Name = name,
-                Color = color,
-            });
-
             colorLabel.Background = new SolidColorBrush(Color.FromRgb(color.R, color.G, color.B));
             RedSlider.Value = color.R;
             GreenSlider.Value = color.G;
@@ -72,18 +64,35 @@ namespace ColorChecker {
                             c.Color.B == myColor.Color.B)
                             .FirstOrDefault();
 
-            stockList.Items.Add(colorName?.Name ?? $"R：{RedSlider.Value} G：{GreenSlider.Value} B：{BlueSlider.Value}");
-            stockMyColor.Add(myColor);
+            stockList.Items.Insert(0,colorName?.Name ?? $"R：{RedSlider.Value} G：{GreenSlider.Value} B：{BlueSlider.Value}");
+            stockMyColor.Insert(0,myColor);
         }
 
         private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            int index = stockList.SelectedIndex;
-            RedSlider.Value = stockMyColor[index].Color.R;
-            GreenSlider.Value = stockMyColor[index].Color.G;
-            BlueSlider.Value = stockMyColor[index].Color.B;
+            try {
+                int index = stockList.SelectedIndex;
+                RedSlider.Value = stockMyColor[index].Color.R;
+                GreenSlider.Value = stockMyColor[index].Color.G;
+                BlueSlider.Value = stockMyColor[index].Color.B;
+                setColor();
+            } catch(Exception) { }
+        }
+
+        private void setColor() {
+            var r = byte.Parse(rValue.Text);
+            var g = byte.Parse(gValue.Text);
+            var b = byte.Parse(bValue.Text);
+            Color color = Color.FromRgb(r, g, b);
+            colorLabel.Background = new SolidColorBrush(color);
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                int index = stockList.SelectedIndex;
+                stockList.Items.RemoveAt(index);
+            } catch (Exception) { }
         }
     }
-
 
 
     public class MyColor {
