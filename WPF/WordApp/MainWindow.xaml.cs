ec1838b50608eab4dd68b6de6c03abcb7a515cdb
33301TomitaKazuyoshi <33301@ojs.ac.jp>
2022-12-02 15:55:50 +0900

    WordApp途中経過①

---
+++ WPF/WordApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WordApp {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btExit_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void btCreate_Click(object sender, RoutedEventArgs e) {
+            var creater = new Creater();
+            frame.Navigate(creater);
+        }
+
+        private void btTest_Click(object sender, RoutedEventArgs e) {
+            var fSelect = new FolderSelect();
+            frame.Navigate(fSelect);
+        }
+    }
+}
---
+++ WPF/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace WpfVMFeedback {
+	public class HelloWorldViewModel : INotifyPropertyChanged {
+		public HelloWorldViewModel() {
+
+		}
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void NotifyPropertyChanged(String info) {
+			if (PropertyChanged != null) {
+				PropertyChanged(this, new PropertyChangedEventArgs(info));
+			}
+		}
+		#endregion
+	}
+}
